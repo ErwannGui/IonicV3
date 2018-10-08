ec27078d1417b5404a632f7393c22efd4d896435
ErwannGui <erwann.guillevic@ynov.com>
2018-10-08 19:54:53 +0200

    api & display
    bug lors de l'affichage des données !
    erreur : films is undefined dans ListPage

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,32 +3,43 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
+import { RestProvider } from '../../providers/rest/rest';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{title: string, year: number, icon: string, image: string}>;
+  films: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider) {
+
+    this.getFilms();
 
     this.items = [];
-    for(let i = 1; i < 11; i++) {
+    
+    for (let i = 1; i <= 10; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: films[i].title,
+        year: films[i].year,
+        image: films[i].poster,
+        icon: 'film'
       });
     }
   }
 
-  itemTapped(event, item) {
+  getFilms() {
+    this.restProvider.getFilms()
+    .then(data => {
+      this.films = data['search'];
+      console.log(this.films);
+    });
+  }
+
+  itemTapped(event, film) {
     this.navCtrl.push(ItemDetailsPage, {
-      item: item
+      film: film
     });
   }
 }
---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+	apiUrl = 'http://www.omdbapi.com/';
+	apiKey = 'd55be316';
+	search = 'avenger';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  getFilms() {
+    return new Promise(resolve => {
+	    this.http.get(this.apiUrl+'?apikey='+this.apiKey+'&s='+this.search).subscribe(data => {
+	      resolve(data);
+	      console.log(data);
+	    }, err => {
+	      console.log(err);
+	    });
+  	});
+  }
+
+}
